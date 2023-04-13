b16d97ac3d374aa51547a0175c9e6eb106654183
Thanh Le <tadev@Thanhs-MBP.local>
2023-04-13 22:49:47 +0300

    Lama Blog

---
+++ api/routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { getPost, addPost, deletePost , getPosts, updatePost } from "../controller/post.js"
+
+
+const router = express.Router()
+
+router.get("/",getPosts)
+router.get("/:id",getPost)
+router.post("/",addPost)
+router.delete("/:id",deletePost)
+router.put("/:id",updatePost)
+
+
+export default router
